360ef1a356251bc7430b8aa39a7c6713188e5a12
coderz <coderz@gmail.com>
2022-11-10 15:34:22 +0600

    Final reload touch test

--- main.js
+++ main.js
@@ -117,13 +117,52 @@ function swipe(rm) {
 
 }
 
+(function () {
+    var touchStartHandler,
+        touchMoveHandler,
+        touchPoint;
+
+    // Only needed for touch events on chrome.
+    if ((window.chrome || navigator.userAgent.match("CriOS"))
+        && "ontouchstart" in document.documentElement) {
+
+        touchStartHandler = function () {
+            // Only need to handle single-touch cases
+            touchPoint = event.touches.length === 1 ? event.touches[0].clientY : null;
+        };
+
+        touchMoveHandler = function (event) {
+            var newTouchPoint;
+
+            // Only need to handle single-touch cases
+            if (event.touches.length !== 1) {
+                touchPoint = null;
+
+                return;
+            }
+
+            // We only need to defaultPrevent when scrolling up
+            newTouchPoint = event.touches[0].clientY;
+            if (newTouchPoint > touchPoint) {
+                event.preventDefault();
+            }
+            touchPoint = newTouchPoint;
+        };
+
+        document.addEventListener("touchstart", touchStartHandler, {
+            passive: false
+        });
+
+        document.addEventListener("touchmove", touchMoveHandler, {
+            passive: false
+        });
+
+    }
+})
+
 window.addEventListener("touchmove", function (e) {
 
-    e.preventDefault();
-    e.stopPropagation();
-    e.stopImmediatePropagation();
-    if (e.cancelable) {
-    }
+    
 
     if (touch.go) {
         swipe()
@@ -132,19 +171,13 @@ window.addEventListener("touchmove", function (e) {
 
 })
 
-document.addEventListener("touchmove", function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    e.stopImmediatePropagation();
+document.addEventListener("touchmove", function (e) {
+
 })
 
 window.addEventListener("touchend", function (e) {
 
-    if (e.cancelable) {
-        e.preventDefault();
-        e.stopPropagation();
-        e.stopImmediatePropagation();
-    }
+
 
     touch.go = false;
     swipe(true)
@@ -152,13 +185,13 @@ window.addEventListener("touchend", function (e) {
 })
 
 window.addEventListener("touchcancill", function (e) {
-    
-    if (e.cancelable) {
-        e.preventDefault();
-        e.stopPropagation();
-        e.stopImmediatePropagation();
-    }
 
     touch.go = false;
     swipe(true)
+})
+
+window.addEventListener("beforeunload", function(e){
+    if(touch.go){
+        e.preventDefault()
+    }
 })
