0ff3b330189995d6cba823958b94caca63296251
coderz <coderz@gmail.com>
2022-11-11 19:17:20 +0600

    🌟 Approve container and manue close contianer system added

--- main.js
+++ main.js
@@ -3,19 +3,23 @@ let shaodw = document.querySelector(".shadow");
 
 // Step select array
 let step = {
-    select: Array(0),
+    select: Array(),
     index: 0,
 
     // Math progress
+    // Frame total length - 1 | becouse in frame class one element not for select
     get prog() {
-        return (100 / frame.length) * step.select.length
+        return (100 / frame.length - 1) * step.select.length
     }
 }
 
 function frame_click(e) {
     const index = Array.from(e.parentElement.children).indexOf(e)
+    step.index = index;
+
     if (e.classList.contains("select")) {
-        step.select.splice(index, 1)
+
+        step.select.splice(index - 1, 1)
 
         // Removing elemtn
         e.classList.remove("select");
@@ -28,8 +32,6 @@ function frame_click(e) {
         step.select.push(e.children[0].innerText)
     }
 
-    step.index = index;
-
     // This print function is for exmple
     console.log(Math.floor(step.prog), "%")
     console.log(step.select)
@@ -37,6 +39,7 @@ function frame_click(e) {
 
 let swip = document.querySelector(".swip");
 let steps = document.querySelector(".step");
+let closeSteps = document.getElementById("closeSteps");
 
 function show_step() {
     shaodw.style.display = "block";
@@ -45,3 +48,27 @@ function show_step() {
 }
 
 // swipe areat on stouch and move
+
+let sugation = document.querySelector('.sugation');
+let approvalCard = document.getElementById("approvalCard")
+let approvalButton = document.getElementById("approvalButton");
+
+approvalButton.addEventListener("click", function () {
+    if (sugation.classList.contains("dn")) {
+        sugation.classList.remove("dn")
+        approvalCard.classList.add("dn")
+        steps.classList.add("down")
+        shaodw.style.display = "none";
+    } else {
+        sugation.classList.add("dn")
+        approvalCard.classList.remove("dn")
+        steps.classList.add("down")
+        shaodw.style.display = "none";
+    }
+    console.log(sugation.classList.contains("dn"))
+})
+
+closeSteps.addEventListener("click", function(){
+    steps.classList.add("down")
+    shaodw.style.display = "none";
+})
